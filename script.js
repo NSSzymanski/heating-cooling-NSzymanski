0c2db960c1cda0195845b5c1c2f006b899da8513
nell.szymanski <nelliesimone@gmail.com>
2023-04-26 17:07:44 -0400

    first commit of heating-cooling lab

---
+++ script.js
@@ -0,0 +1,14 @@
+let actualTemp;
+
+let desiredTemp;
+
+actualTemp = 72;
+desiredTemp = 72;
+
+if (actualTemp > desiredTemp) {
+  console.log("Run A/C");
+} else if (actualTemp < desiredTemp) {
+  console.log("Run heat");
+} else {
+  console.log("Standby");
+}
